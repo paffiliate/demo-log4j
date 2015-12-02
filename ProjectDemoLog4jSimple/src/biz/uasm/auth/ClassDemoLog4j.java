cec2e58c71202bda404172e960b5d714865ec314
paffiliate <paffiliate@ukr.net>
2015-12-02 12:12:45 +0400

    New main class

---
+++ ProjectDemoLog4jSimple/src/biz/uasm/auth/ClassDemoLog4j.java
@@ -0,0 +1,11 @@
+package biz.uasm.auth;
+
+public class ClassDemoLog4j {
+    public ClassDemoLog4j() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        ClassDemoLog4j classDemoLog4j = new ClassDemoLog4j();
+    }
+}
