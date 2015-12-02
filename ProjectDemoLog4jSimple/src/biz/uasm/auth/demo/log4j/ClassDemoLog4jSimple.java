d4a64f39387e6619593051a2c6f0c1fa33242209
paffiliate <paffiliate@ukr.net>
2015-12-02 12:04:06 +0400

    Create main class ClassDemoLog4jSimple

---
+++ ProjectDemoLog4jSimple/src/biz/uasm/auth/demo/log4j/ClassDemoLog4jSimple.java
@@ -0,0 +1,11 @@
+package biz.uasm.auth.demo.log4j;
+
+public class ClassDemoLog4jSimple {
+    public ClassDemoLog4jSimple() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        ClassDemoLog4jSimple classDemoLog4jSimple = new ClassDemoLog4jSimple();
+    }
+}
