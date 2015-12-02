644afbea6e289498635a03c5bff94cdd0df6cf39
paffiliate <paffiliate@ukr.net>
2015-12-02 12:05:58 +0400

    ClassDemoLog4jSimple add some functions

--- ProjectDemoLog4jSimple/src/biz/uasm/auth/demo/log4j/ClassDemoLog4jSimple.java
+++ ProjectDemoLog4jSimple/src/biz/uasm/auth/demo/log4j/ClassDemoLog4jSimple.java
@@ -7,6 +7,10 @@ public class ClassDemoLog4jSimple {
 
     public static void main(String[] args) {
         ClassDemoLog4jSimple classDemoLog4jSimple = new ClassDemoLog4jSimple();
+<<<<<<< 87d22acbc01b6c22cd8e535ae081d1c62c4d629a
         System.out.println("Other test");
+=======
+        System.out.println("test");
+>>>>>>> 9d32bad26e4626f2d8015c4f212edec3790e3137
     }
 }
