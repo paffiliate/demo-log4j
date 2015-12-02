19b3e9d8879ceb89e6f9b3e47601fd1516b38e5c
paffiliate <paffiliate@ukr.net>
2015-12-02 12:16:18 +0400

    New classes

---
+++ ProjectDemoLog4jSimple/src/biz/uasm/auth/demo/log4j/ClassDemoLog4j.java
@@ -0,0 +1,11 @@
+package biz.uasm.auth.demo.log4j;
+
+public class ClassDemoLog4j {
+    public ClassDemoLog4j() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        ClassDemoLog4j classDemoLog4j = new ClassDemoLog4j();
+    }
+}
